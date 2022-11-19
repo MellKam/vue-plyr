472f8d55f738a79f8a8515006ea0d2fa073648ae
MelKam <melkam464@gmail.com>
2022-11-19 21:18:11 +0000

    chore: remove vite plugin for vue

--- packages/vue-plyr/vite.config.ts
+++ packages/vue-plyr/vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
 	build: {
@@ -13,5 +12,4 @@ export default defineConfig({
 			},
 		},
 	},
-	plugins: [vue()],
 });
